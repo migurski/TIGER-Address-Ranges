172f7bf641da9590571bd6497b53ead584aa74a9
Michal Migurski <mike-git@teczno.com>
2013-09-16 17:44:59 -0500

    Adapted insert script for edgeinfo data

--- insert.sh
+++ insert.sh
@@ -1,7 +1,23 @@
 #!/bin/sh -ex
 
-for SHP in ??/*_ranges.shp; do 
+for SRC in ??/tl_2013_?????_ranges.shp; do 
 
-    shp2pgsql -a -s 3857 -W Windows-1252 $SHP tiger_2013_ranges
-
+    FIPS=${SRC#??/tl_2013_}
+    FIPS=${FIPS%_ranges.shp}
+    ZIP="tl_2013_${FIPS}_edges.zip"
+    URL="ftp://ftp.census.gov:21//geo/tiger/TIGER2013/EDGES/${ZIP}"
+    
+    curl -o $ZIP -L $URL
+    unzip -quj $ZIP
+    
+    SHP="${ZIP%.zip}.shp"
+    
+    shp2pgsql -a -n -W Windows-1252 $SHP tiger_2013_edgeinfo
+    
+    rm -f "${ZIP%.zip}.shx"
+    rm -f "${ZIP%.zip}.prj"
+    rm -f "${ZIP%.zip}.dbf"
+    rm -f "${ZIP%.zip}.shp.xml"
+    rm -f $ZIP $SHP
+    
 done
